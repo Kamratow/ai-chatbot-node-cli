172c4303ad5a046177e4063072f00900ad2a8d57
Kamil Ratowski <kamilratowski90@gmail.com>
2024-09-04 00:52:40 +0200

    Added readline to make the program more interactive and allow getting prompts content from terminal written by the user

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import OpenAI from "openai";
 import "dotenv/config";
+import readline from "readline";
 
 const openai = new OpenAI();
 
-openai.chat.completions
-  .create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a helpful assistant. Try to be concise and straight to the point.",
-      },
-      { role: "user", content: "What is a LLM?" },
-    ],
-    model: "gpt-4o-mini",
-  })
-  .then((result) => console.log(result.choices[0].message.content));
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+userInterface.prompt();
+
+userInterface.on("line", async (input) => {
+  await openai.chat.completions
+    .create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant. Try to be concise and straight to the point.",
+        },
+        { role: "user", content: input },
+      ],
+      model: "gpt-4o-mini",
+    })
+    .then((result) => {
+      console.log(result.choices[0].message.content);
+      userInterface.prompt();
+    })
+    .catch((error) => console.log(error));
+});
